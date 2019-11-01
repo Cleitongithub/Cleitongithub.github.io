0800350a2bde0494edf722ed76065b694f3e40f1
cleiton garcia palma <cleitongarciapalma@gmail.com>
2019-10-31 23:47:15 -0300

    Suavizei o menu com jquery

--- js/codigo.js
+++ js/codigo.js
@@ -4,10 +4,19 @@ document.querySelector('.abrir').onclick = function() {
 document.querySelector('.fechar').onclick = function() {
     document.documentElement.classList.remove('ativo');
 };
-window.alert("Bem Vindo a minha Pagina conheça um pouco sobre mim!");
+window.alert("Bem Vindo a minha página conheça um pouco sobre mim!");
 function grande(x) {
     x.style.width = "13%";
 }
 function normal(x) {
     x.style.width = "12%";
 }
+$('.menu-principal a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffetset = $(id).offset().top,
+        menuHeight = $('.menu-principal').innerHeight();
+    $('html, body').animate({
+        scrollTop: targetOffetset -menuHeight
+    }, 1000);
+});
